c1686a3fe053c63a32361c9709155b5a6d95ff22
MoncozGC <jadepengc@163.com>
2020-09-21 23:39:17 +0800

    database-查询重复的电子邮件

---
+++ DataBase/182-查询电子邮件.sql
@@ -0,0 +1,20 @@
+编写一个 SQL 查询，查找 Person 表中所有重复的电子邮箱。
+
+示例：
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+根据以上输入，你的查询应返回以下结果：
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+
+select distinct p1.Email from Person p1, Person p2 where p1.Email=p2.Email and p1.Id != p2.Id;
