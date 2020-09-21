08e5eac7b949eba303420edcb612906b399301eb
MoncozGC <jadepengc@163.com>
2020-09-21 23:32:46 +0800

    添加sql练习语句

---
+++ DataBase/183-从不订购的客户.sql
@@ -0,0 +1,34 @@
+某网站包含两个表，Customers 表和 Orders 表。编写一个 SQL 查询，找出所有从不订购任何东西的客户。
+
+Customers 表：
+
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders 表：
+
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+例如给定上述表格，你的查询应返回：
+
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+SELECT name FROM Customers WHERE id NOT IN (SELECT CustomerId FROM Orders);
+
+# 注
+# NOT In 的结果中不能含有NULL,如果含有最终结果则为NULL.
+# IN 中含有NULL不影响最终结果.
