eb8a817f4aea9f32838bc942b5be0a8ebac16e42
MoncozGC <jadepengc@163.com>
2021-04-23 22:53:54 +0800

    QB1

---
+++ test/algorithms/CharAtTest.java
@@ -0,0 +1,19 @@
+package algorithms;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by MoncozGC on 2021/4/21
+ */
+public class CharAtTest {
+    public static void main(String[] args) {
+        Set<Character> occ = new HashSet<Character>();
+        String s = "abcasdas";
+        for (int i = 1; i < s.length(); ++i) {
+
+            System.out.println(occ.remove(s.charAt(i - 1)) + " " + i);
+            System.out.println(occ.contains(s.charAt(i - 1)) + " " + i);
+        }
+    }
+}
