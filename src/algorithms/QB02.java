7a5bfe0ec7bc320154eb1b3b6ecf736237615f16
MoncozGC <jadepengc@163.com>
2021-04-26 19:43:23 +0800

    链表ListNode

---
+++ src/algorithms/QB02.java
@@ -0,0 +1,53 @@
+package algorithms;
+
+/**
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示范 1: 输入：l1 = [2,4,3], l2 = [5,6,4] 输出：[7,0,8] 解释：342 + 465 = 807.
+ * 示例 2: 输入：l1 = [0], l2 = [0] 输出: [0]
+ * 示例 3: 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9] 输出：[8,9,9,9,0,0,0,1]
+ * Created by MoncozGC on 2021/4/25
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution02 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //生成ListNode链表对象，链表的值为0，没有指向的节点
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        //carry 表示进位数
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            //新节点的数值为sum % 10
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+
+            cur = cur.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+        return pre.next;
+    }
+}
